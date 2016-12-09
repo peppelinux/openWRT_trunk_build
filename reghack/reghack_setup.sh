631a297290077a2a66d32141ea5bb1a4ef9440b8
Giuseppe De Marco <demarcog83@gmail.com>
2016-12-09 12:36:15 +0100

    Update reghack_setup.sh

--- reghack/reghack_setup.sh
+++ reghack/reghack_setup.sh
@@ -1,8 +1,8 @@
 cd /tmp/
 wget http://luci.subsignal.org/~jow/reghack/reghack.mips.elf
 chmod +x ./reghack.mips.elf
-./reghack.mips.elf /lib/modules//ath.ko
-./reghack.mips.elf /lib/modules//cfg80211.ko
+./reghack.mips.elf /lib/modules/*/ath.ko
+./reghack.mips.elf /lib/modules/*/cfg80211.ko
 reboot
 
 uci set wireless.@wifi-device[0].country=US
