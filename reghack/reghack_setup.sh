a44ba71f11a464fd0fd2986c46a23bdaa970c726
Giuseppe De Marco <demarcog83@gmail.com>
2016-12-09 12:36:01 +0100

    Create reghack_setup.sh

---
+++ reghack/reghack_setup.sh
@@ -0,0 +1,9 @@
+cd /tmp/
+wget http://luci.subsignal.org/~jow/reghack/reghack.mips.elf
+chmod +x ./reghack.mips.elf
+./reghack.mips.elf /lib/modules//ath.ko
+./reghack.mips.elf /lib/modules//cfg80211.ko
+reboot
+
+uci set wireless.@wifi-device[0].country=US
+uci set wireless.@wifi-device[0].txpower=30
